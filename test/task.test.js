d4081de52a83c7a1ea61fce3a2758449954938d5
Mikito Takada <mikito.takada@gmail.com>
2013-05-15 15:00:32 -0700

    Switch to using readable-stream for 0.10.x streams

--- test/task.test.js
+++ test/task.test.js
@@ -2,31 +2,29 @@ var fs = require('fs'),
     uglify = require('../lib/file-tasks/uglify.js'),
     wrap = require('../lib/file-tasks/wrap-commonjs-web.js');
 
-var wrapper = wrap(),
-    uglifier = uglify();
-
-var s = fs.createReadStream(__dirname + '/fixtures/single-file/simple.js');
-
-s.pipe(wrapper.stdin);
-
-wrapper.stdout.pipe(uglifier.stdin);
-uglifier.stdout.pipe(process.stdout);
-
-/*
 var tasks = [
   wrap,
-  uglify
+//  uglify,
+  process
 ];
 
 var stream = { stdout: fs.createReadStream(__dirname + '/fixtures/single-file/simple.js') };
 
 tasks.forEach(function(task) {
-  var instance = task();
-
+  var instance = task;
+  // item is either a object (e.g. process) with .stdout/.stdin
+  // or a function that returns an object
+  if(typeof instance == 'function') {
+    instance = task();
+  }
   stream.stdout.pipe(instance.stdin);
+  // if there is a stderr, pipe that - this avoids issues where the task fails to stderr
+  // and the stdout is not flushed due to buffering
+  if(instance.stderr) {
+    instance.stderr.pipe(process.stderr);
+  }
 
   stream = instance;
 });
 
-stream.stdout.pipe(process.stdout);
-*/
+
