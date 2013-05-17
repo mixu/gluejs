bd4884b8acde5e8c0a48e83e5b828452ee4294fc
Mikito Takada <mikito.takada@gmail.com>
2013-05-16 20:47:57 -0700

    Write the basic package creation runner

---
+++ test/runner/package.test.js
@@ -0,0 +1,10 @@
+var util = require('util'),
+    Tree = require('../../lib/tree.js');
+
+var task = require('../../lib/runner/package-commonjs'),
+    tree = new Tree();
+
+tree.add(__dirname+'/../fixtures/complex-package/');
+
+task(tree);
+
